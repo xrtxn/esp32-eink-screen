efb6e0161b845589ce723aa2052e6380b8a5ad02
xrtxn <mihok.martin@protonmail.com>
2025-12-28 20:15:49 +0100

    use new flake from leighleighleigh/esp-rs-nix

--- flake.nix
+++ flake.nix
@@ -1,26 +1,66 @@
 {
-  description = "A nix-shell for developing with Rust on Xtensa+RISCV ESP32 targets";
+  description = "ESP32 thesis project using esp-rs-nix for Rust development";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    esp-rs-nix = {
+      url = "github:leighleighleigh/esp-rs-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-        shell = pkgs.callPackage ./shell.nix {};
-        esp-rs-src = pkgs.lib.sources.cleanSource ./.;
-      in {
-        package = rec {
-          inherit shell;
-          default = shell;
-          esp-rs = pkgs.callPackage "${esp-rs-src}/esp-rs/default.nix" {};
-        };
-        devShells.default = shell;
-      }
-    );
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      esp-rs-nix,
+      ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          esp-rs = esp-rs-nix.packages.${system}.esp-rs;
+        in
+        {
+          default = pkgs.mkShell {
+            name = "esp32-thesis";
+
+            buildInputs = [
+              esp-rs
+              pkgs.rustup
+              pkgs.espflash
+              pkgs.rust-analyzer
+              pkgs.pkg-config
+              pkgs.stdenv.cc
+              pkgs.systemdMinimal
+            ];
+
+            shellHook = ''
+              # Add a prefix to the shell prompt
+              export PS1="(esp-rs)$PS1"
+
+              # This variable is important - it tells rustup where to find the esp toolchain,
+              # without needing to copy it into your local ~/.rustup/ folder.
+              export RUSTUP_TOOLCHAIN=${esp-rs}
+
+              # Set RUST_SRC_PATH for build-std to find the library sources
+              export RUST_SRC_PATH=${esp-rs}/lib/rustlib/src/rust/library
+
+              # Override where Cargo looks for stdlib sources (needed for build-std)
+              export __CARGO_TESTS_ONLY_SRC_ROOT=${esp-rs}/lib/rustlib/src/rust/library
+            '';
+          };
+        }
+      );
+    };
 }
